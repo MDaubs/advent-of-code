91a1d1d92623474e8aee4b190a86b62c00a1b60d
Matthew Daubert <mdaubert@gmail.com>
2016-12-09 08:54:05 -0500

    Allow dynamically defined keypads

--- day2part2.cr
+++ day2part2.cr
@@ -1,19 +1,20 @@
 require "string_scanner"
 
-alias Key = UInt8
-alias Instruction = Key -> Key
+alias Key = Char
 
-MoveUp    = ->(key : Key) { key <= 3 ? key : key - 3 }
-MoveDown  = ->(key : Key) { key >= 7 ? key : key + 3 }
-MoveLeft  = ->(key : Key) { key % 3 == 1 ? key : key - 1 }
-MoveRight = ->(key : Key) { key % 3 == 0 ? key : key + 1 }
+enum Instruction
+  MoveUp
+  MoveDown
+  MoveLeft
+  MoveRight
+end
 
 struct Instructions
   MAP = {
-    'U' => MoveUp,
-    'D' => MoveDown,
-    'L' => MoveLeft,
-    'R' => MoveRight
+    'U' => Instruction::MoveUp,
+    'D' => Instruction::MoveDown,
+    'L' => Instruction::MoveLeft,
+    'R' => Instruction::MoveRight
   }
 
   @lines : Array(Array(Instruction))
@@ -28,32 +29,101 @@ struct Instructions
 end
 
 class Keypad
-  def initialize(@instructions : Instructions)
+  def initialize(@grid : Array(Array(Key)), @instructions : Instructions)
   end
 
   def code
-    starting_key = 5.to_u8
+    starting_key = '5'
 
     @instructions.lines.map { |line|
       starting_key = line.reduce(starting_key) { |key, instruction|
-        instruction.call(key)
+        case instruction
+        when Instruction::MoveUp
+          key_above(key)
+        when Instruction::MoveDown
+          key_below(key)
+        when Instruction::MoveLeft
+          key_left_of(key)
+        else
+          key_right_of(key)
+        end || key
       }
     }
+  end
+
+  def coordinates_of(key : Key)
+    @grid.each_with_index do |keys, row|
+      keys.each_with_index do |candidate_key, col|
+        if candidate_key == key
+          return {row, col}
+        end
+      end
+    end
+
+    raise "Keypad does not include #{key}."
+  end
+
+  def key_above(key : Key)
+    row, col = coordinates_of(key)
+
+    if row == 0
+      nil
+    else
+      @grid[row - 1][col]
+    end
+  end
+
+  def key_below(key : Key)
+    row, col = coordinates_of(key)
+
+    if row == @grid.size - 1
+      nil
+    else
+      @grid[row + 1][col]
+    end
+  end
+
+  def key_left_of(key : Key)
+    row, col = coordinates_of(key)
+
+    if col == 0
+      nil
+    else
+      @grid[row][col - 1]
+    end
+  end
+
+  def key_right_of(key : Key)
+    row, col = coordinates_of(key)
+
+    if col == @grid[row].size - 1
+      nil
+    else
+      @grid[row][col + 1]
+    end
   end
 end
 
 require "spec"
 
-describe "first example" do
-  it "produces the correct bathroom code" do
-    keypad = Keypad.new(Instructions.new("ULL\nRRDDD\nLURDL\nUUUUD"))
-    keypad.code.should eq([1, 9, 8, 5])
-  end
-end
+describe "Day 2" do
+  grid = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
+  ]
 
-describe "challenge example" do
-  it "produces the correct bathroom code" do
-    keypad = Keypad.new(Instructions.new(File.read("day2.input")))
-    keypad.code.should eq([8, 2, 9, 5, 8])
+  describe "first example" do
+    it "produces the correct bathroom code" do
+      keypad = Keypad.new(grid, Instructions.new("ULL\nRRDDD\nLURDL\nUUUUD"))
+      keypad.code.should eq(['1', '9', '8', '5'])
+    end
+  end
+
+  describe "challenge example" do
+    it "produces the correct bathroom code" do
+      keypad = Keypad.new(grid, Instructions.new(File.read("day2.input")))
+      keypad.code.should eq(['8', '2', '9', '5', '8'])
+    end
   end
 end
