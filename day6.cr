5adc9e33517b8a2934676a1249d8434c6266283b
Matthew Daubert <mdaubert@gmail.com>
2016-12-14 19:44:37 -0500

    Add part 2 to day 6 solution

--- day6.cr
+++ day6.cr
@@ -1,25 +1,49 @@
 require "spec"
 
 module Day6
-  def self.receive(messages : String)
-    messages.strip.split("\n").map(&.chars).transpose.map(&->most_common(Array(Char))).join
-  end
+  alias Strategy = Array({Char, UInt32}) -> {Char, UInt32}
+  MOST_COMMON = Strategy.new(&.last)
+  LEAST_COMMON = Strategy.new(&.first)
 
-  private def self.most_common(chars : Array(Char))
-    chars
-      .each_with_object(Hash(Char, UInt32).new(0_u32)) { |char, counts| counts[char] += 1 }
-      .to_a.sort_by(&.last).last.first
+  def self.receive(strategy : Strategy, messages : String)
+    messages
+      .strip
+      .split("\n")
+      .map(&.chars)
+      .transpose
+      .map { |chars| chars.each_with_object(Hash(Char, UInt32).new(0_u32)) { |char, counts| counts[char] += 1 } }
+      .map(&.to_a)
+      .map(&.sort_by(&.last))
+      .map(&strategy)
+      .map(&.first)
+      .join
   end
 end
 
 describe "Day 6" do
   it "error corrects the sample messages" do
     Day6
-      .receive("eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar")
+      .receive(
+        Day6::MOST_COMMON,
+        "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar"
+      )
       .should eq("easter")
   end
 
   it "error corrects the challenge message" do
-    Day6.receive(File.read("day6.input")).should eq("kqsdmzft")
+    Day6.receive(Day6::MOST_COMMON, File.read("day6.input")).should eq("kqsdmzft")
+  end
+
+  it "modified error corrects the sample messages" do
+    Day6
+      .receive(
+        Day6::LEAST_COMMON,
+        "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar"
+      )
+      .should eq("advent")
+  end
+
+  it "modified error corrects the challenge message" do
+    Day6.receive(Day6::LEAST_COMMON, File.read("day6.input")).should eq("tpooccyo")
   end
 end
