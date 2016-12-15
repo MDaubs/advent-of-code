d9f99841ed28e9a14fb6bff0198529ec9697988d
Matt Daubert <matt.daubert@meyouhealth.com>
2016-12-15 12:03:17 -0500

    Add part 2 to day 7 solution

--- day7.cr
+++ day7.cr
@@ -3,30 +3,34 @@ struct IP
   end
 
   def supports_tls?
-    abbas_inside_hypernet, abbas_outside_hypernet = abbas.partition(&.hypernet)
+    abbas_inside_hypernet, abbas_outside_hypernet = special_groups(4) { |(a, b, c, d)| a == d && b == c && a != b }.partition(&.hypernet)
     abbas_inside_hypernet.none? && abbas_outside_hypernet.any?
   end
 
-  private def abbas
-    inside_hypernet = false
-
-    (0..@characters.size-5).map { |index|
-      a, b, c, d = @characters[index..index+4]
-
-      if a == '['
-        inside_hypernet = true; nil
-      elsif a == ']'
-        inside_hypernet = false; nil
-      elsif a == d && b == c && a != b
-        ABBA.new(hypernet: inside_hypernet)
-      end
-    }.compact
+  def supports_ssl?
+    babs, abas = special_groups(3) { |(a, b, c)| a == c && a != b }.partition(&.hypernet)
+    abas.any? { |aba| babs.any? { |bab| bab.characters == [aba.characters[1], aba.characters[0], aba.characters[1]] }}
   end
 
-  struct ABBA
-    getter hypernet
+  private def special_groups(chars)
+    inside_hypernet = false
 
-    def initialize(@hypernet : Bool)
+    (0..@characters.size-chars)
+      .map { |index|
+        if @characters[index] == '['
+          inside_hypernet = true; nil
+        elsif @characters[index] == ']'
+          inside_hypernet = false; nil
+        elsif yield(@characters.chars[index..index+chars-1])
+          SpecialGrouping.new(@characters.chars[index..index+chars-1], inside_hypernet)
+        end
+      }.compact
+  end
+
+  struct SpecialGrouping
+    getter characters, hypernet
+
+    def initialize(@characters : Array(Char), @hypernet : Bool)
     end
   end
 end
@@ -56,5 +60,29 @@ describe "Day 7" do
       .map { |c| IP.new(c) }
       .count(&.supports_tls?)
       .should eq(110)
+  end
+
+  it "concludes aba[bab]xyz supports SSL" do
+    IP.new("aba[bab]xyz").supports_ssl?.should eq(true)
+  end
+
+  it "concludes xyx[xyx]xyx does not support SSL" do
+    IP.new("xyx[xyx]xyx").supports_ssl?.should eq(false)
+  end
+
+  it "concludes aaa[kek]eke supports SSL" do
+    IP.new("aaa[kek]eke").supports_ssl?.should eq(true)
+  end
+
+  it "concludes zazbz[bzb]cdb supports SSL" do
+    IP.new("zazbz[bzb]cdb").supports_ssl?.should eq(true)
+  end
+
+  it "counts the number of IPs that support SSL in the challenge input" do
+    File
+      .each_line("day7.input")
+      .map { |c| IP.new(c) }
+      .count(&.supports_ssl?)
+      .should eq(242)
   end
 end
